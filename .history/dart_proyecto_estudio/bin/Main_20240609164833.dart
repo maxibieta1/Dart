6298d465f961fdc2f00fc3e970edb46dbcf1d05b
maximiliano <maxitto@gmail.com>
2024-06-09 16:50:46 -0400

    correciones menores

---
+++ .history/dart_proyecto_estudio/bin/Main_20240609164833.dart
@@ -0,0 +1,6 @@
+import 'package:dart_preyecto_estudio/dart_preyecto_estudio.dart'
+    as dart_preyecto_estudio;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_preyecto_estudio.calculate()}!');
+}
