1262d1c6f25f3c28e129fbcebb19fed262e096bf
maximiliano <maxitto@gmail.com>
2024-06-09 16:34:00 -0400

    comienzo de programacion en dart

---
+++ dart_preyecto_estudio/test/dart_preyecto_estudio_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_preyecto_estudio/dart_preyecto_estudio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
